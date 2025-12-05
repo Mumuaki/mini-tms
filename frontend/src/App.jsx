e429d27be5184b1919a7ba28b3f26282212a1566
Mumuaki <151684806+Mumuaki@users.noreply.github.com>
2025-12-05 01:38:13 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import MyTrucks from './pages/MyTrucks';
+import FreightList from './pages/FreightList';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/trucks" element={<MyTrucks />} />
+          <Route path="/freights" element={<FreightList />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
